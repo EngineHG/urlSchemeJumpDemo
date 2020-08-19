7c51d37d77880759425ff6173c7c88c0e8d0e1d3
JY <604362118@qq.com>
2020-08-19 16:44:34 +0800

    代码上传

---
+++ UrlSchemeDemo/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  UrlSchemeDemo
+//
+//  Created by 黄国坚 on 2020/8/10.
+//  Copyright © 2020 黄国坚. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let urlSchemes: [String] = ["hailuo://resolve?domain=mianfanqiang123",
+                                "hailuo://resolve?domain=OKExGroup_Chinese",
+                                "hailuo://resolve?domain=flychatin",
+                                "hailuo://proxy?server=103.39.214.69&port=9000&secret=ddd65ebdc80f5a056dc9ea8354cd53b8",
+                                "hailuo://authorization?openId=11",
+                                "hailuo://pay?openId=321",
+                                "hailuo://recharge?openId=231",
+                                "hailuo://withdrawal?openId=312312"]
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach(self.urlSchemes, id: \.self){
+                    JumpBtn(title: $0)
+                }
+            }
+        }
+    }
+    
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+struct JumpBtn: View {
+    
+    let title: String
+    
+    var body: some View {
+        Button(action: {
+            self.jumpScheme(self.title)
+        }) {
+            Text(title)
+        }
+    }
+    
+    private func jumpScheme(_ urlString: String){
+        UIApplication.shared.open(URL(string: urlString)!)
+    }
+}
